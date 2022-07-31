1588848336f43288690bfcdcc9ffc11ca42a8a83
Deksio99 <jordanoskidavid@gmail.com>
2022-07-31 13:11:05 +0200

    Added ChessBoard

--- app/src/main/java/com/fict/chesspuzzle/ui/theme/ui/MainActivity.kt
+++ app/src/main/java/com/fict/chesspuzzle/ui/theme/ui/MainActivity.kt
@@ -3,18 +3,25 @@ package com.fict.chesspuzzle.ui.theme.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fict.chesspuzzle.ui.theme.ui.ui.theme.BoardColors
 import com.fict.chesspuzzle.ui.theme.ui.ui.theme.ChessPuzzleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ChessPuzzleTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,22 +29,63 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ChessPuzzleTheme {
-        Greeting("Android")
+        Scaffold(topBar = {
+            TopAppBar {
+                Text(text = "Chess Puzzle")
+            }
+
+        }) {
+
+        }
+        Column(
+            Modifier
+                .paddingFromBaseline(68.dp, 0.dp),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+
+            for (y in 0 until 8) {
+                Row {
+                    for (x in 0 until 8) {
+                        val white = y % 2 == x % 2
+                        val color = if (white) BoardColors.lightSquare else BoardColors.darkSquare
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .background(color)
+                                .aspectRatio(1.0f)
+                        ) {
+                            if (y == 7) {
+                                Text(
+                                    text = "${'A' + x}",
+                                    modifier = Modifier.align(Alignment.BottomEnd),
+                                    style = MaterialTheme.typography.caption,
+                                    color = Color.Black.copy(0.5f)
+                                )
+                            }
+                            if (x == 0) {
+                                Text(
+                                    text = "${8 - y}",
+                                    modifier = Modifier.align(Alignment.TopStart),
+                                    style = MaterialTheme.typography.caption,
+                                    color = Color.Black.copy(0.5f)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/fict/chesspuzzle/ui/theme/ui/ui/theme/Color.kt
+++ app/src/main/java/com/fict/chesspuzzle/ui/theme/ui/ui/theme/Color.kt
@@ -6,3 +6,13 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val blueVariant = Color(0xFF003b8e)
+
+object BoardColors {
+    val lastMoveColor = blueVariant.copy(alpha = 0.5f)
+    val lightSquare = Color(0xFFF0D9B5)
+    val darkSquare = Color(0xFF946f51)
+    val checkColor = Color.Red.copy(alpha = 0.5f)
+    val attackColor = Color.Red.copy(alpha = 0.5f)
+    val moveColor = lastMoveColor
+}
